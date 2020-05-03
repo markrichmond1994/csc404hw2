c91fb8a319d971d45e0cf1ed526876870b6f948d
markrichmond1994 <60485080+markrichmond1994@users.noreply.github.com>
2020-05-03 19:56:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+//Still don't know if we  need anything from our old main.js here....
+
+
+"use strict";
+
+const GradeInput = require("./GradeInput");
+const QualifiedStudents = require("./QualifiedStudents");
+
+GradeInput.getGrades();
+
+QualifiedStudents.getThreshold();
