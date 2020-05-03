7150fc79c9bdfab9b3930d3500d441d7b257f20a
markrichmond1994 <60485080+markrichmond1994@users.noreply.github.com>
2020-05-03 19:56:30 -0400

    Add files via upload

---
+++ students/main.js
@@ -0,0 +1,78 @@
+"use strict";
+
+//instructions. Type following into visual studio code terminal...
+//cd students
+//npm init --yes
+//npm install -S
+//node main.js
+//use http://localhost:3000 to view website in your web browser
+
+const port = 3000,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  router = require("./router"),
+  contentTypes = require("./contentTypes"),
+  utils = require("./utils");
+
+router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.htm);
+  utils.getFile("views/index.html", res);
+});
+
+router.get("/courses.html", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/courses.html", res);
+});
+
+router.get("/contact.html", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/contact.html", res);
+});
+
+router.post("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/thanks.html", res);
+});
+
+router.get("/graph.png", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.png);
+  utils.getFile("public/images/graph.png", res);
+});
+
+router.get("/people.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/people.jpg", res);
+});
+
+router.get("/product.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/product.jpg", res);
+});
+
+router.get("/confetti_cuisine.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/confetti_cuisine.css", res);
+});
+
+router.get("/bootstrap.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/bootstrap.css", res);
+});
+
+router.get("/confetti_cuisine.js", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.js);
+  utils.getFile("public/js/confetti_cuisine.js", res);
+});
+
+router.get("/confetti_cuisine.js", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.js);
+  utils.getFile("public/js/GradeInput.js", res);
+});
+
+router.get("/confetti_cuisine.js", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.js);
+  utils.getFile("public/js/QualifiedStudents.js", res);
+});
+
+http.createServer(router.handle).listen(port);
+console.log(`Use http://localhost:3000 to view website. The server is listening on port number: ${port}`);
